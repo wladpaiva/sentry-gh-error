21a7404986c231829a72c19e90f5e4824cb81620
Wlad Paiva <desenvolvedormg@gmail.com>
2025-04-24 09:25:22 -0300

    fix: pipe stdout in Playwright web server configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   ],
   webServer: {
     command: "npm run dev",
+    stdout: "pipe",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
